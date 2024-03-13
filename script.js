a5070ba0f8a517a5f47faf56601cf7763f7c3bf5
Namix23 <98861500+Namix23@users.noreply.github.com>
2024-03-13 01:49:50 -0400

    create a function parameter

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ function calculateDiscount(){
   const discountPercentage = .2;
   const fullPrice = 49.99;
   const discount = fullPrice * discountPercentage;
-  console.log(discount);
+  console.log(Math.round(discount));
 }
 calculateDiscount();
+
+
+//function parameters
+function calculateDiscount(discountPercentage, fullPrice) {
+  const discount = discountPercentage * fullPrice;
+}
+calculateDiscount(0.15, 300);
