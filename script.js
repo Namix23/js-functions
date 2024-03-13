e873ad9a6213235226c649ee6c9c0002144c8656
Namix23 <98861500+Namix23@users.noreply.github.com>
2024-03-12 23:58:18 -0400

    add console.log()

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ function calculateDiscount(){
   const discountPercentage = .2;
   const fullPrice = 49.99;
   const discount = fullPrice * discountPercentage;
+  console.log(discount);
 }
 calculateDiscount();
